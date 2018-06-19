2a58a04e09f6a159895722986663af3c67bd3659
Pedro Francisco de Sousa Neto <pedrokra@gmail.com>
2018-06-19 16:51:43 -0300

    Criando mais um sample

--- app/src/main/java/br/com/redcode/revealpassword/sample/MainActivity.kt
+++ app/src/main/java/br/com/redcode/revealpassword/sample/MainActivity.kt
@@ -2,6 +2,7 @@ package br.com.redcode.revealpassword.sample
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import br.com.redcode.revealpassword.library.showOrHidePassword
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,6 +12,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         editTextWithImageForRevealPassword.changeIcon(android.R.drawable.ic_delete)
+
         editTextWithImageForRevealPassword.changeHint(getString(R.string.app_name))
+
+        buttonShowHidePassword.setOnClickListener { editTextSampleFunction.showOrHidePassword() }
     }
 }
